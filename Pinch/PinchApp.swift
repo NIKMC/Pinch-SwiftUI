f6f23a89d27bc0756f628c7aa24e17d10823c746
NIKMC <nikmc10@gmail.com>
2023-06-08 10:38:16 +0300

    init app

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Ivan Nikitin on 30.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
